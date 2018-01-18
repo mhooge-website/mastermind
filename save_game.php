784d180af08fbeb70f06456780ab4d0aebf155dc
Mikkel Hooge Sørensen <Mikkel Hooge Srensen>
2018-01-18 17:49:35 +0100

    Collatz db stuff

--- save_game.php
+++ save_game.php
@@ -1,14 +1,11 @@
 <?php
 header("Content-Type: application/json; charset=UTF-8");
 include('../../scripts/connect_db.php');
-
 $jsonGame = json_decode($_GET["game"], false);
 $conn = connect_db("mhso_grpro");
-
 if($jsonGame->status == "dead") deleteGame();
 else if($jsonGame->id == null) insertValues();
 else updateValues();
-
 function insertValues() {
     global $conn, $jsonGame;
     if (!($stmt = $conn->prepare("INSERT INTO mastermind_games(id, name, status, round, repeat_pins, empty_pins, autoplace_results, 
@@ -16,10 +13,8 @@ function insertValues() {
         http_response_code(500);
         echo "Prepare failed: (" . $conn->errno . ") " . $conn->error;
     }
-
     $time = time();
     $id = hash("md5", $time . $jsonGame->name);
-
     if (!$stmt->bind_param("sssiiiiis", $id, $jsonGame->name, $jsonGame->status, $jsonGame->round, $jsonGame->repeat, 
     $jsonGame->empty, $jsonGame->autoresults, $jsonGame->creator, $jsonGame->code)) {
         http_response_code(500);
@@ -30,10 +25,8 @@ function insertValues() {
         http_response_code(500);
         echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
     }
-
     echo $id;
 }
-
 function updateValues() {
     global $conn, $jsonGame;
     if (!($stmt = $conn->prepare("UPDATE mastermind_games SET name=?, status=?, round=?, repeat_pins=?, empty_pins=?, autoplace_results=?,
@@ -41,7 +34,6 @@ function updateValues() {
         http_response_code(500);
         echo "Prepare failed: (" . $conn->errno . ") " . $conn->error;
     }
-
     if (!$stmt->bind_param("ssiiiiiss", $jsonGame->name, $jsonGame->status, $jsonGame->round, $jsonGame->repeat, 
     $jsonGame->empty, $jsonGame->autoresults ,$jsonGame->creator, $jsonGame->code, $jsonGame->id)) {
         http_response_code(500);
@@ -54,14 +46,12 @@ function updateValues() {
     }
     http_response_code(204);
 }
-
 function deleteGame() {
     global $conn, $jsonGame;
     if (!($stmt = $conn->prepare("DELETE FROM mastermind_games WHERE id=?"))) {
         http_response_code(500);
         echo "Prepare failed: (" . $conn->errno . ") " . $conn->error;
     }
-
     if (!$stmt->bind_param("s", $jsonGame->id)) {
         http_response_code(500);
         echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
@@ -73,5 +63,4 @@ function deleteGame() {
     }
     http_response_code(204);
 }
-
 ?>
